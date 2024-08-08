75526f4953aee7fea96a3f23b556c4364922f7f6
Dev Bhandari <devbh789@gmail.com>
2024-08-09 01:58:55 +0530

    Updated username validation

--- src/utils/validation/user.validation.js
+++ src/utils/validation/user.validation.js
@@ -3,7 +3,13 @@ import customValidationErrorProvider from "./index.validation.js";
 
 const username = zod
     .string()
-    .min(3, { message: "Username should be of minimum 3 characters" });
+    .min(3, { message: "Username should be of minimum 3 characters" })
+    .refine((s) => !s.includes(" "), "Username should not contain spaces")
+    .refine((value) => {
+        const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+        return !regex.test(value);
+    }, "Username should not contain symbols");
+
 const email = zod.string().email({ message: "Invalid email" });
 const password = zod
     .string()
