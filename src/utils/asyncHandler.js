7be9d21e3c2c42ec4df32ad81a3e51fc8869548a
Dev Bhandari <devbh789@gmail.com>
2025-10-26 14:36:24 +0530

    Fixed status code error in asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (fn) => async (req, res, next) => {
     try {
         await fn(req, res, next);
     } catch (error) {
-        res.status(error.code || 500).json({
+        res.status(error.status_code).json({
             sucess: false,
             message: error.message,
         });
