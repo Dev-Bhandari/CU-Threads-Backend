9cf9d9f5d91e90fe4229ffbc3ab6ddfa203cd4e6
Dev Bhandari <devbh789@gmail.com>
2025-10-27 20:14:38 +0530

    Reverted status code change

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (fn) => async (req, res, next) => {
     try {
         await fn(req, res, next);
     } catch (error) {
-        res.status(error.status_code).json({
+        res.status(error.statusCode).json({
             sucess: false,
             message: error.message,
         });
